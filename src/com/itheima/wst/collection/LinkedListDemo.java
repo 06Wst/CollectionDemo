91639774ff8bdbc4bd4dcb55f3b3016a277a08f4
W sir <15053305396@163.com>
2026-08-08 10:25:20 +0800

    26.8.8

---
+++ src/com/itheima/wst/collection/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package com.itheima.wst.collection;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> l1=new LinkedList<>();
+        l1.add("a");
+        l1.add("b");
+        l1.add("c");
+
+        System.out.println(l1);
+    }
+}
